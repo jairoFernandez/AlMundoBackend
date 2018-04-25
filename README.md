82fb2e4e709bc6d8fa92e32f6caa5c699dbf6ea2
jairo <jairog12@outlook.com>
2018-04-25 00:23:28 -0500

    add cors support

--- README.md
+++ README.md
@@ -2,7 +2,17 @@
 
 1. Express
 2. Nodemon
+3. Cors
 
 # Deploy in heroku
 Endpoint: https://api-almundo.herokuapp.com/api
 - Hotels: /hotel
+
+# Links
+Demo en vivo
+- Front: [https://jairofernandez.github.io/AlMundoFrontend/](https://jairofernandez.github.io/AlMundoFrontend/)
+- Back: [https://api-almundo.herokuapp.com/api](https://api-almundo.herokuapp.com/api)
+
+## Repositorios:
+- Backend: [https://github.com/jairoFernandez/AlMundoBackend](https://github.com/jairoFernandez/AlMundoBackend)
+- Frontend: [https://github.com/jairoFernandez/AlMundoFrontend](https://github.com/jairoFernandez/AlMundoFrontend)
--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ var express = require('express');
 var server = express();
 var bodyParser  = require("body-parser");
 var path = require('path');
+var cors = require('cors');
 
 server.use(bodyParser.urlencoded({ extended: false }));
 server.use(bodyParser.json());
 
 server.use(express.static(path.join(__dirname, 'assets'))); // Add folder assets for public use
-
+server.use(cors());
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ });
+ 
 /**
  * Router home with service
  */
